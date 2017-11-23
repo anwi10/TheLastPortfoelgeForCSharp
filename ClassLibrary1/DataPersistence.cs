e19cbda4573cc6ed79190690edbadf69366342ae
Anders Winter <awint15@student.sdu.dk>
2017-11-23 17:30:00 +0100

    Newest version

---
+++ ClassLibrary1/DataPersistence.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+
+namespace ClassLibraryDrawApp
+{
+    public class DataPersistence
+    {
+        //Constant storage name
+        private const string storageName = "MyTest.txt";
+
+        //Persistence data
+        private string firstName;
+        private string surName;
+        private string eMail;
+        private string phoneNr;
+        private DateTime date;
+        private string serialNumber;
+
+        public DataPersistence(string firstName, string surName, string eMail,string phoneNr, DateTime date, string serialNumber)
+        {
+            this.firstName = firstName;
+            this.surName = surName;
+            this.date = date;
+            this.eMail = eMail;
+            this.phoneNr = phoneNr;
+            this.date = date;
+            this.serialNumber = serialNumber;
+
+        }
+
+        public async void SaveInformation()
+        {
+            //Using a filestream
+            FileStream fs = null;
+            FileInfo fi = null;
+            //Try/finally to close the stream
+            try
+            {
+                // Create a reference to a file.
+
+                //fi = new FileInfo(@"c:\tmp\MyTest.txt");
+                // Actually create the file.
+                
+
+               await Task.Run(() => fs = new FileStream(storageName,FileMode.OpenOrCreate,FileAccess.ReadWrite));
+                using (StreamWriter dataWriter = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    dataWriter.Write(firstName);
+                    dataWriter.Write(surName);
+                    dataWriter.Write(eMail);
+                    dataWriter.Write(phoneNr);
+                    dataWriter.Write(date.ToString());
+                    dataWriter.Write(serialNumber);
+                }
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Dispose();
+                    fi.Delete();
+                }
+            }
+        }
+    }
+}
--- EnterDrawApp/MainPage.xaml.cs
+++ EnterDrawApp/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ClassLibraryDrawApp;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +27,21 @@ namespace EnterDrawApp
         public MainPage()
         {
             this.InitializeComponent();
+
+        }
+
+        private async void DoneButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            string fN = firstNameTextBox.Text;
+            string sN = surNameTextBox.Text;
+            string eM = eMailTextBox.Text;
+            string pN = phoneNrTextBox.Text;
+            DateTime dB = dateOfBirthDatePicker.Date.DateTime;
+            string serialN = serialNrTextBox.Text;
+
+            DataPersistence datePer = new  DataPersistence(fN,sN,eM,pN,dB,serialN);
+
+            await Task.Run(() => datePer.SaveInformation());
         }
     }
 }
