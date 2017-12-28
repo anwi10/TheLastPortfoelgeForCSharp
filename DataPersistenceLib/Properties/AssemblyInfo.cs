ac8ffe0fc35e0294e346a1d6b2f121b3f2556c2a
Anders Winter <awint15@student.sdu.dk>
2017-12-28 07:09:56 +0100

    Deleted leftover files and folders, changed library name

--- DataPersistenceLib/Properties/AssemblyInfo.cs
+++ DataPersistenceLib/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DataValidationLib")]
+[assembly: AssemblyTitle("DataPersistenceLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataValidationLib")]
+[assembly: AssemblyProduct("DataPersistenceLib")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
